cc6947a043b185319634c3ab22c6e3ba376868c3
Manisharath1 <rathmanisha889@gmail.com>
2025-04-25 14:58:46 +0530

    update login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,6 +25,7 @@ const Login = () => {
         headers: {
           'Content-Type': 'application/json',
         },
+        credentials: 'include',
         body: JSON.stringify({
           username,
           password,
